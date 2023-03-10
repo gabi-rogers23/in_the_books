b84ac854f193d43b2aa5d8619d2aac78d0d68c0d
Ricardo Torres <ricardo_torres2013@yahoo.com>
2023-03-09 18:25:36 -0600

    login css

--- src/components/bookDetails.js
+++ src/components/bookDetails.js
@@ -22,6 +22,7 @@ const BookDetails = () => {
   }
 
   return (
+<<<<<<< HEAD
     <div className="">
       <div className="">
         <img src={book.bookImage} />
@@ -71,6 +72,32 @@ const BookDetails = () => {
       >
         Add to Cart
       </button>
+=======
+    <div class="bookDetails">
+        <div><img src={book.bookImage}/></div>
+        <div id="bookTitle">
+                <b>Title:</b> {book.title}
+              </div>
+              <div id="bookPrice">
+                <b>Price:</b> {book.price}
+              </div>
+              <div id="bookAuthor">
+                <b>Author:</b> {book.authorFirstName} {book.authorLastName}
+              </div>
+              <div id="bookDes">{book.description}</div><p/>
+              <div id="bookAuthorImage"><img src={book.authorImage}/></div>
+              <div id="authorAbout">About {book.authorFirstName} {book.authorLastName}: {book.authorBio}</div>
+              {book.tags.length > 0 && (
+                <div>
+                  <b>Tags:</b>{book.tags.map((tag) => {
+                      return <div key={tag.tagId} onClick={((e)=> buttonHandler(e, `/booktag/${tag.tag}`))}>{tag.tag} </div>;
+                    })}
+                
+                </div>
+              )}
+              <button onClick={((e)=>buttonHandler(e, "/books"))}>Back</button><button>Add to Cart</button>
+
+>>>>>>> a6dd20b (css stuff)
     </div>
   );
 };
